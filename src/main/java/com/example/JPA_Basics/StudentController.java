f6377452e87c89d336f3f2cc74faf3343dad2e05
durjyo8888 <kdurjyodhan141@gmail.com>
2023-07-16 23:22:26 +0530

    done

--- src/main/java/com/example/JPA_Basics/StudentController.java
+++ src/main/java/com/example/JPA_Basics/StudentController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class StudentController {
     @Autowired
@@ -21,5 +23,28 @@ public class StudentController {
             return new ResponseEntity("Student not found", HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity(student, HttpStatus.FOUND);
+    }
+    @DeleteMapping("/delete-by-id")
+    public ResponseEntity deleteStudentById(@RequestParam int rollNo){
+        String deleteId = studentService.deleteStudentById(rollNo);
+        return new ResponseEntity(deleteId,HttpStatus.OK);
+    }
+    @PutMapping("/update-age")
+    public ResponseEntity UpdateAgeById(@RequestParam("age") int age,@RequestParam("id") int Id){
+        String student = studentService.UpdateAgeById(age, Id);
+        return new ResponseEntity(student, HttpStatus.OK);
+    }
+    @GetMapping("/get-all-student-greater-then-x")
+    public ResponseEntity GetAllStudentAgeGreaterThenX(@RequestParam int x){
+        List<Student> student = studentService.GetAllStudentAgeGreaterThenX(x);
+        return new ResponseEntity(student, HttpStatus.FOUND);
+    }
+    @DeleteMapping("/delete-all")
+    public ResponseEntity deleteAllStudent(){
+        String student = studentService.deleteAllStudent();
+        if(student == null){
+            return new ResponseEntity("Students not exist", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(student, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/JPA_Basics/StudentService.java
+++ src/main/java/com/example/JPA_Basics/StudentService.java
@@ -3,6 +3,8 @@ package com.example.JPA_Basics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +22,40 @@ public class StudentService {
             return optionalStudent.get();
         }
         return null;
+    }
+
+    public String deleteStudentById(int rollNo) {
+        studentRepository.deleteById(rollNo);
+        return "Student delete successfully";
+    }
+
+    public String UpdateAgeById(int age, int id) {
+        Optional<Student> optionalStudent = studentRepository.findById(id);
+
+        if(optionalStudent.isPresent()){
+            optionalStudent.get().setAge(age);
+        }
+        return "Student age Updated successfully";
+    }
+
+    public List<Student> GetAllStudentAgeGreaterThenX(int x) {
+        List<Student> studentList = studentRepository.findAll();
+        List<Student> students = new ArrayList<>();
+
+        for(Student student : studentList){
+            if(student.getAge() >= x){
+                students.add(student);
+            }
+        }
+        return students;
+    }
+
+    public String deleteAllStudent() {
+        List<Student> studentList = studentRepository.findAll();
+        if(studentList.size() == 0){
+            return null;
+        }
+        studentRepository.deleteAll();
+        return "All Students delete successfully";
     }
 }
